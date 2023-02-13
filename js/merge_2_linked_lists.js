a3af39979ec7c0ada95fdf315a5024375741f59f
davidfdomejiab <davidfdomejiab@gmail.com>
2023-02-13 13:29:09 -0500

    merge two linked listts

---
+++ js/merge_2_linked_lists.js
@@ -0,0 +1,29 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+exports.LinkedList = LinkedList;
+
+function mergingLinkedLists(linkedListOne, linkedListTwo) {
+  let auxSet =  new Set();
+  while(linkedListOne) {
+    auxSet.add(linkedListOne.value)
+    linkedListOne = linkedListOne.next;
+  }
+
+  while(linkedListTwo) {
+    if(auxSet.has(linkedListTwo.value)) {
+      return linkedListTwo
+    }
+
+    linkedListTwo = linkedListTwo.next
+  }
+
+  return null;
+}
+// Do not edit the line below.
+exports.mergingLinkedLists = mergingLinkedLists;
