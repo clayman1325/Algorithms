9770a155b8e1bb86737aa753af7cfc0f322a4589
davidfdomejiab <davidfdomejiab@gmail.com>
2023-02-13 13:57:12 -0500

    fix typos and console logs in the power sum problem

--- js/power_set.js
+++ js/power_set.js
@@ -4,15 +4,13 @@ function powerset(array) {
   for(let i=0; i < array.length; i++) {
     const element = array[i];
     let newArray = [];
-    console.log(powerSet)
+
     for(let j=0; j < powerSet.length; j++) {
       const powerSetCopy = [...powerSet]
       powerSetElement = powerSetCopy[j]
       powerSetElement.push(element)
       newArray = newArray.concat(powerSetElement)
-      // console.log(newArray)
     }
-    // powerSet = powerSet.concat(newArray);
   }
 
   return powerSet;
