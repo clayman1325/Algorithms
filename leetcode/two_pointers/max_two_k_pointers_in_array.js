430af121fea74cfd03c3aec718dbd8895ee54716
David Mejia <davidfdomejiab@gmail.com>
2023-05-17 21:48:10 -0500

    add the two pointer solution to the max number of operations in an array that should sum to target with two elements

---
+++ leetcode/two_pointers/max_two_k_pointers_in_array.js
@@ -0,0 +1,18 @@
+var maxOperations = function(nums, k) {
+    nums.sort((a,b) => a - b);
+
+    let total = 0;
+    let l = 0;
+    let r = nums.length - 1;
+
+    while(l < r) {
+        if(nums[l] + nums[r] == k) {
+            total++; l++; r--;
+        } else {
+            const moveL = nums[l] + nums[r] < k;
+            (moveL) ? l++ : r--;
+        }
+    }
+
+    return total;
+};
