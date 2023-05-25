07f801e45052cb6760ae95dfb0166634299f9af9
David Mejia <davidfdomejiab@gmail.com>
2023-05-24 19:20:37 -0500

    practicing two pointers strategy with the max area watter container

---
+++ leetcode/two_pointers/water_container_3.js
@@ -0,0 +1,23 @@
+var maxArea = function (height) {
+    var leftIndex = 0;
+    var rightIndex = height.length - 1;
+    var maxStoredWater = 0;
+
+    while (leftIndex < rightIndex) {
+        const leftHeight = height[leftIndex];
+        const rightHeight = height[rightIndex];
+        const width = rightIndex - leftIndex;
+        const smallerHeight = leftHeight < rightHeight ? leftHeight : rightHeight;
+        const storedWater = width * smallerHeight;
+
+        maxStoredWater = storedWater > maxStoredWater ? storedWater : maxStoredWater;
+
+        if(height[leftIndex] <= height[rightIndex]) {
+            leftIndex++
+        } else {
+            rightIndex--
+        }
+    }
+
+    return maxStoredWater;
+};
